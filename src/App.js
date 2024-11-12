cae9a5a7d1da785d3d7935f5380b187b8407925f
mahuma-pixel <69123574+mahuma-pixel@users.noreply.github.com>
2024-11-12 12:43:42 +0200

    Corrections made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="mailto:mahoomah.curl@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mahuma Ribane
+          </a>{" "}
+          and is open-sourced on {" "} <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          > GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
